6c20b98604fa14ea819e42d239179d15784c0b4d
Tara Briggs <tebriggs4@gmail.com>
2017-04-02 22:02:39 +0000

    assignment-18-sensitivedata complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,11 @@ class PostsController < ApplicationController
     # We declare an instance variable @posts and assign it a collection of Post objects 
     # using the all method provided by ActiveRecord. all returns a collection of  Post objects.
     @posts = Post.all
+    @posts.each_with_index do |post, index|
+      if (index % 5 == 0)
+        post.title = "SPAM"
+      end
+    end
   end
 
   def show

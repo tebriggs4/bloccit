211b51af4804e32498025d8431774b27a97328f1
Tara Briggs <tebriggs4@gmail.com>
2017-04-05 16:58:03 +0000

    assignment-20-question complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,7 +34,16 @@ posts = Post.all
         body: RandomData.random_paragraph
     )
 end
- 
+
+100.times do
+    Question.create!(
+        title:  RandomData.random_sentence,
+        body:   RandomData.random_paragraph,
+        resolved: false
+    )
+end
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Question.count} questions created"

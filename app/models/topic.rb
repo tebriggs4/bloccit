4b4c8a3f643d7f6ab6a981c0a385aa55b379bd9f
Tara Briggs <tebriggs4@gmail.com>
2017-04-11 01:50:42 +0000

    assignment-23-validation complete

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,6 @@
 class Topic < ActiveRecord::Base
     has_many :posts, dependent: :destroy
     # Because comments already depend on posts, they will also be deleted when a topic is deleted.
+    validates :name, length: { minimum: 5 }, presence: true
+    validates :description, length: { minimum: 15 }, presence: true
 end

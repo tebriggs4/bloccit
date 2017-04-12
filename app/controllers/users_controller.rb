18eb0f0c7d9ccdaebed4d1d828e3beeac3cabdaf
Tara Briggs <tebriggs4@gmail.com>
2017-04-12 17:06:12 +0000

    checkpoint-25-signup complete

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+    
+    # create is the action that is called when the new view's form is submitted with valid attributes.
+    def create
+        # We create a new user with new and then set the corresponding attributes from the params hash.
+        @user = User.new
+        @user.name = params[:user][:name]
+        @user.email = params[:user][:email]
+        @user.password = params[:user][:password]
+        @user.password_confirmation = params[:user][:password_confirmation]
+ 
+        # We save the new user to the database. If the database save is successful, we add a flash message
+        # and then redirect the user to the root path. Otherwise, we display an error and render the new view.
+        if @user.save
+            flash[:notice] = "Welcome to Bloccit #{@user.name}!"
+            redirect_to root_path
+        else
+            flash.now[:alert] = "There was an error creating your account. Please try again."
+            render :new
+        end
+    end
+
+end
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+    # We create a hash of attributes named new_user_attributes so we can use them easily throughout our spec.
+    let(:new_user_attributes) do
+        {
+            name: "BlocHead",
+            email: "blochead@bloc.io",
+            password: "blochead",
+            password_confirmation: "blochead"
+        }
+    end
+    
+    # We test the new action for HTTP success when issuing a GET. The first test expects the response
+    # to return an HTTP response code of 200. The second test expects new to instantiate a new user.
+    describe "GET new" do
+        it "returns http success" do
+            get :new
+            expect(response).to have_http_status(:success)
+        end
+ 
+        it "instantiates a new user" do
+            get :new
+            expect(assigns(:user)).to_not be_nil
+        end
+    end
+    
+    # We test the create action for HTTP success when issuing a POST with  new_user_attributes set as the params hash.
+    describe "POST create" do
+        it "returns an http redirect" do
+            post :create, user: new_user_attributes
+            expect(response).to have_http_status(:redirect)
+        end
+ 
+        # We test that the database count on the users table increases by one when we issue a POST to create.
+        it "creates a new user" do
+            expect{
+                post :create, user: new_user_attributes
+           }.to change(User, :count).by(1)
+        end
+ 
+        # We test that we set user.name properly when creating a user.
+        it "sets user name properly" do
+            post :create, user: new_user_attributes
+            expect(assigns(:user).name).to eq new_user_attributes[:name]
+        end
+ 
+        # We test that we set user.email properly when creating a user.
+        it "sets user email properly" do
+            post :create, user: new_user_attributes
+            expect(assigns(:user).email).to eq new_user_attributes[:email]
+        end
+ 
+        # We test that we set user.password properly when creating a user.
+        it "sets user password properly" do
+            post :create, user: new_user_attributes
+            expect(assigns(:user).password).to eq new_user_attributes[:password]
+        end
+ 
+        # We test that we set user.password_confirmation properly when creating a user.
+        it "sets user password_confirmation properly" do
+            post :create, user: new_user_attributes
+            expect(assigns(:user).password_confirmation).to eq new_user_attributes[:password_confirmation]
+        end
+    end
+end

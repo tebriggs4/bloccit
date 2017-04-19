f08c39c38b0decd1dd54047cbecbc7755ce94aa0
Tara Briggs <tebriggs4@gmail.com>
2017-04-19 22:25:36 +0000

    assignment-27-scopes complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,8 @@ class Post < ActiveRecord::Base
     
     # The default_scope will order all posts by their created_at date, in descending order, with the most recent posts displayed first.
     default_scope { order('created_at DESC') }
+    scope :ordered_by_title, -> { order('title DESC') }
+    scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
     
     validates :title, length: { minimum: 5 }, presence: true
     validates :body, length: { minimum: 20 }, presence: true

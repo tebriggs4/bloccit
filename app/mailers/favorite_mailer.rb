7ced48dc324e5f54327f6f3729d30ef668d091ba
Tara Briggs <tebriggs4@gmail.com>
2017-04-27 19:37:36 +0000

    assignment-31-favorites complete

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -16,4 +16,18 @@ class FavoriteMailer < ApplicationMailer
         # and any cc or bcc information - and prepares the email to be sent.
         mail(to: user.email, subject: "New comment on #{post.title}")
     end
+    
+    def new_post(post)
+ 
+        # We set three different headers to enable conversation threading in different email clients.
+        headers["Message-ID"] = "<posts/#{post.id}@your-app-name.example>"
+        headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+        headers["References"] = "<post/#{post.id}@your-app-name.example>"
+ 
+        @post = post
+        
+        # The mail method takes a hash of mail-relevant information - the subject the  to address, the from (we're using the default), 
+        # and any cc or bcc information - and prepares the email to be sent.
+        mail(to: post.user.email, subject: "You created a new post and will not follow it #{post.title}")
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,8 @@ class Post < ActiveRecord::Base
     has_many :votes, dependent: :destroy
     has_many :favorites, dependent: :destroy
     
+    after_create :create_favorite
+    
     # The default_scope will order all posts by their rank inn descending order.
     default_scope { order('rank DESC') }
     
@@ -38,6 +40,11 @@ class Post < ActiveRecord::Base
         age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
         new_rank = points + age_in_days
         update_attribute(:rank, new_rank)
+    end
+    
+    def create_favorite
+        Favorite.create(post: self, user: self.user)
+        FavoriteMailer.new_post(self).deliver_now 
     end
    
 end
